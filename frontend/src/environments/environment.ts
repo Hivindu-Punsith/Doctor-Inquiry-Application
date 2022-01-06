9161d81a4fd3dd920f201eb90ccc152ff6517f29
Hivindu Punsith <86920745+Hivindu-Punsith@users.noreply.github.com>
2022-01-06 09:10:45 +0530

    2022.01.06

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const baseUrl=`http://127.0.0.1:8000/api/`;
+export const baseUrl=`http://backend.com/api/`;
 
 /*
  * For easier debugging in development mode, you can import the following file
